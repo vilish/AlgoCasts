0ff4eb5ceb2ec25611890ee5f39a182e3a57cd3f
SPADTCC <psingh9@dtcc.edu>
2019-07-06 14:31:14 -0400

    reverse int simple string-array solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let isNegative = false;
+    if (Math.sign(n) === -1) {
+        isNegative = true;
+        n = Math.abs(n);
+    }
+
+    let reversed = '';
+
+    reversed = n
+        .toString()
+        .split('')
+        .reduce((reversed, char) => char + reversed, '');
+
+
+    console.log('reversed   ' + reversed);
+
+    return parseInt(reversed) * (isNegative ? -1 : 1);
+
+}
+
+reverseInt(100);
 
 module.exports = reverseInt;
