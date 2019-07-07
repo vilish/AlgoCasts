6ca75e31a2821ff341337a2f05a0e5635e71e5dd
SPADTCC <psingh9@dtcc.edu>
2019-07-07 11:53:56 -0400

    array chunks solution - loop array and assign chunks and chunksArr if chunks reaches size or end of loop

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,27 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+
+    let chunks = [];
+    let chunksArr = [];
+    let counter = 0;
+
+    //loop all the array index
+    for (let index in array) {
+        //assign chunks
+        chunks[index % size] = array[index];
+
+        //if chunks reaches the max size or end of loop -> add chunks to chunksArr
+        if (chunks.length === size || index == array.length - 1) {
+            chunksArr[counter++] = chunks;
+            chunks = [];
+        }
+    }
+    // return the chunksArr
+    return chunksArr
+}
+
+
 
 module.exports = chunk;
