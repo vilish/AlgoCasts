0609e2885ceff6af1a7cf03f201ffe1a0d630c81
SPADTCC <psingh9@dtcc.edu>
2019-07-07 16:10:51 -0400

    capitalize each word in a paragraph by array split slice solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,15 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+
+    let capitalizeStrArr = [];
+
+    for (let word of str.split(' ')) {
+        capitalizeStrArr.push(word[0].toUpperCase() + word.slice(1));
+    }
+
+    return capitalizeStrArr.join(' ');
+}
 
 module.exports = capitalize;
