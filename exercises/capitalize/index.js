6b1361eb41bb23647bd8e1cb675ad9a5c6b509b5
SPADTCC <psingh9@dtcc.edu>
2019-07-07 19:00:28 -0400

    capitalize each word -> loop string and capitalize first letter or one after space

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,15 +7,52 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {
-
-    let capitalizeStrArr = [];
-
-    for (let word of str.split(' ')) {
-        capitalizeStrArr.push(word[0].toUpperCase() + word.slice(1));
+const capitalize = (str) => {
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+        if (i == 0 || str[i - 1] === ' ')
+            result += str[i].toUpperCase();
+        else
+            result += str[i];
     }
-
-    return capitalizeStrArr.join(' ');
+    return result;
 }
+
+
+// solution without split
+// const capitalize = (str) => {
+//     let word = '';
+//     let wordArr = [];
+//     for (let letter of str) {
+//         if (letter === ' ' ) {
+//             wordArr = wordArr.concat(word[0].toUpperCase() + word.slice(1));
+//             word = '';
+//             continue;
+//         } else {
+//             word += letter;
+//         }
+//     }
+//     if (word !== '')
+//         wordArr = wordArr.concat(word[0].toUpperCase() + word.slice(1));
+
+//     console.log(wordArr);
+//     return wordArr.join(' ');
+// }
+
+capitalize("hello word there");
+
+// solution using str to array, then split by space to get a word, 
+// uppercase first letter of word and assign to new arr with uncganged rest of the word.
+// function capitalize(str) {
+
+//     let capitalizeStrArr = [];
+
+//     for (let word of str.split(' ')) {
+//         // if (word.trim() !== '') // when there are many spaces in sentence, ignore empty word
+//         capitalizeStrArr.push(word[0].toUpperCase() + word.slice(1));
+//     }
+
+//     return capitalizeStrArr.join(' ');
+// }
 
 module.exports = capitalize;
