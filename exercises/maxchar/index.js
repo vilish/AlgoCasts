123748eb5cb4329755d6bea19b8f5b7a4fe021df
SPADTCC <psingh9@dtcc.edu>
2019-07-07 07:27:30 -0400

    maxCHar in a string solution using one & two for loop

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,42 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// const maxChar = (str) => {
+//     const chars = {};
+
+//     let max = 0;
+//     let maxChar;
+
+//     for (let character of str) {
+//         chars[character] = chars[character] + 1 || 1;
+//         if (chars[character] > max && character !== maxChar) {
+//             max = chars[character];
+//             maxChar = character;
+//         }
+
+//     }
+//     return maxChar;
+// }
+
+
+const maxChar = (str) => {
+    const chars = {};
+
+    let max = 0;
+    let maxChar;
+
+    for (let character of str) {
+        chars[character] = chars[character] + 1 || 1;
+    }
+
+    for (let index in chars) {
+        if (chars[index] > max) {
+            max = chars[index];
+            maxChar = index;
+        }
+
+    }
+    return maxChar;
+}
 
 module.exports = maxChar;
--- exercises/maxchar/test.js
+++ exercises/maxchar/test.js
@@ -1,14 +1,17 @@
 const maxChar = require('./index');
 
-test('maxChar function exists', () => {
-  expect(typeof maxChar).toEqual('function');
-});
+describe('', () => {
 
-test('Finds the most frequently used char', () => {
-  expect(maxChar('a')).toEqual('a');
-  expect(maxChar('abcdefghijklmnaaaaa')).toEqual('a');
-});
+  test('maxChar function exists', () => {
+    expect(typeof maxChar).toEqual('function');
+  });
 
-test('Works with numbers in the string', () => {
-  expect(maxChar('ab1c1d1e1f1g1')).toEqual('1');
-});
+  test('Finds the most frequently used char', () => {
+    expect(maxChar('a')).toEqual('a');
+    expect(maxChar('abcdefghijklmnaaaaa')).toEqual('a');
+  });
+
+  test('Works with numbers in the string', () => {
+    expect(maxChar('ab1c1d1e1f1g1')).toEqual('1');
+  });
+})
