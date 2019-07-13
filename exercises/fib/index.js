80acede96dfbf173c1aa5087795507950e1296bc
SPADTCC <psingh9@dtcc.edu>
2019-07-12 21:42:57 -0400

    fib number iterative solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,31 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+
+    if (n === 0 || n === 1)
+        return n;
+
+    let element1 = 0,
+        element2 = 1;
+
+    for (let i = 1; i < n; i++) {
+        num = element1 + element2;
+        element1 = element2;
+        element2 = num;
+    }
+    // console.log(num);
+    return num;
+}
+
+// fib(2);
+// fib(3);
+// fib(4);
+// fib(5);
+// fib(6);
+// fib(7);
+// fib(8);
+// fib(9);
+// fib(39);
 
 module.exports = fib;
