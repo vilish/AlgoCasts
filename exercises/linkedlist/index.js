d3cbb5d4d0457000691dbc2e875dfab93719995a
vilish <vilish@gmail.com>
2019-07-16 06:15:32 -0400

    Added Node class, Linklist class with some methods

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,55 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
 
-class LinkedList {}
+    constructor(data, next) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
-module.exports = { Node, LinkedList };
+class LinkedList {
+
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data) {
+        let node = new Node(data);
+        node.data = data;
+        node.next = this.head;
+        this.head = node;
+    }
+
+    size() {
+        let counter = 0;
+        let node = this.head;
+        while (node) {
+            node = node.next;
+            counter++;
+        }
+        return counter;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        let node = this.head;
+        while (node && node.next) {
+            node = node.next;
+        }
+        return node;
+    }
+}
+
+const l = new LinkedList();
+// expect(l.size()).toEqual(0);
+l.insertFirst(1);
+l.insertFirst(2);
+l.insertFirst(3);
+console.log(l.size());
+
+module.exports = {Node, LinkedList};
