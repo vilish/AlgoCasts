25c5e16cf7f9c2e8c0ba60494a5171cc9604ae6f
SPADTCC <psingh9@dtcc.edu>
2019-07-21 18:15:25 -0400

    node implemenetation for a tree and Add/remove method for childrens

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,19 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+
+    let slow = list.getFirst(); // list.head;
+    let fast = list.getFirst(); // list.head;
+    while (n-- > 0) {
+        fast = fast.next;
+    }
+    while (fast.next) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return slow;
+
+}
 
 module.exports = fromLast;
