50234d063dd703c1846c309d4af088368046e780
SPADTCC <psingh9@dtcc.edu>
2019-07-26 08:18:33 -0400

    bubble and selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,10 +2,33 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let lesser = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = lesser;
+            }
+        }
+    }
+    return arr;
 }
 
 function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let indexOfMin = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j;
+            }
+        }
+        if (i !== indexOfMin) {
+            let lesser = arr[i];
+            arr[i] = arr[indexOfMin];
+            arr[indexOfMin] = lesser;
+        }
+    }
+    return arr;
 
 }
 
@@ -17,4 +40,9 @@ function merge(left, right) {
 
 }
 
-module.exports = { bubbleSort, selectionSort, mergeSort, merge };
+module.exports = {
+    bubbleSort,
+    selectionSort,
+    mergeSort,
+    merge
+};
